619cdd5099cea7241741a4d2bd8ff85fd4f5138c
ardo <alfredoardo07@gmail.com>
2024-10-31 11:42:45 +0700

    asas
    wswew

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
+import { AdminComponent } from './admin/admin.component';
+
 
 export const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "admin", component: AdminComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "login", component: LoginComponent }
 ];
